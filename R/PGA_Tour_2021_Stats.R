8b550e37193101b96e811af9e1a0d64d466db623
Tin Hoang <tinhoang@Tins-MBP.T-mobile.com>
2022-01-02 13:24:56 -0800

    document the package

---
+++ R/PGA_Tour_2021_Stats.R
@@ -0,0 +1,15 @@
+
+#' PGA Tour 2021 Stats
+#'
+#' A dataset containing stats from the 2021 Season sourced from Kaggle
+#'
+#' @format A data frame of 37044 rows and 5 columns
+#' \describe{
+#'  \item{Player_Name}{Character value containing names of players}
+#'  \item{Category}{Character value containing five major categories: Putting, Approach the Green, Around the Green, Off the Tee and Scoring}
+#'  \item{Sub_Category}{Character value containing different sub categories for each major category}
+#'  \item{Measure_Type}{Character value containing different measure types for each category/sub_category. This can be Number of Rounds played, Average Distance and so on}
+#'  \item{Value}{Numeric value according to each Measure_Type}
+#' }
+#' @source <https://www.kaggle.com/lilcobeeee/2021-pga-tour-stats/>
+"PGA_Tour_2021_Stats"
